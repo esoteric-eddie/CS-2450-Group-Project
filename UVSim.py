520380dfd4dc4a314d593290d6e439d72132e9e6
Gigashmirt <shmirt3003@gmail.com>
2025-02-06 13:45:52 -0700

    Added functions in main to UVSim class
    main() now creates an object of UVSim class called sim and uses it to call functions.

--- UVSim.py
+++ UVSim.py
@@ -23,6 +23,83 @@ class UVSim:
         #Initialize the program counter to 00 (starting execution at the first instruction)
         self.program_counter = 0
 
+    #pass fetch a memory location, get the operation code
+    def fetch_word(self, memory_index):
+        #memory should be whatever the name of the variable that our 0-99 memory array is called. return memory at index.
+        word = self.memory[memory_index]
+        #set operation code to the first two numbers, operand to the last two numbers.
+        word_str = str(word)
+        operation_code = int(word_str[:2])
+        operand = int(word_str[2:4])
+        #return operation code and operand
+        return [operation_code, operand]
+
+    def execute(self):
+        #start at the first memory location, starting operation code is nothing, starting operand is nothing.
+        #first two digits of word
+        operation_code = 00
+        #second two digits of word
+        operand = 00
+        #increment the counter unless branch function, which sets this to false
+        increment_counter = True
+        while operation_code != 43:
+            fetched_word = self.fetch_word(self.program_counter)
+            operation_code = int(fetched_word[0])
+            operand = int(fetched_word[1])
+
+            #check if operation is equal to a relevant operation
+            #relevant operations are 10, 11, 20, 21, 30, 31, 32, 33, 40, 41, 42, 43
+            match operation_code:
+                case 10:
+                    #Call READ
+                    self.memory[operand] = int(input("Enter a number: "))
+                    #print("READ")
+                case 11:
+                    #Call WRITE
+                    print(f"Output: {self.memory[operand]}")
+                    #print("WRITE")
+                case 20:
+                    #Call LOAD
+                    self.accumulator = self.memory[operand]
+                    #print("LOAD")
+                case 21:
+                    #Call STORE
+                    self.memory[operand] = self.accumulator
+                    print("STORE")
+                case 30:
+                    #Call ADD
+                    print("ADD")
+                case 31:
+                    #Call SUBTRACT
+                    print("SUBTRACT")
+                case 32:
+                    #Call MULTIPLY
+                    print("MULTIPY")
+                case 33:
+                    #Call DIVIDE
+                    print("DIVIDE")
+                case 40:
+                    #Call BRANCH
+                    print("BRANCH")
+                    increment_counter = False
+                case 41:
+                    #Call BRANCHNEG
+                    print("BRANCHNEG")
+                    increment_counter = False
+                case 42:
+                    #Call BRANCHZERO
+                    print("BRANCHZERO")
+                    increment_counter = False
+                case 43:
+                    #Call Halt function, but this code will stop executing anyways after halt because of while loop
+                    print("HALT")
+                case default:
+                    print("Not an operation code")
+
+            #increment program_counter if increment counter is true
+            if increment_counter == True:
+                self.program_counter += 1
+
 # Test the implementation
 if __name__ == "__main__":
     # Create and test UVSim
